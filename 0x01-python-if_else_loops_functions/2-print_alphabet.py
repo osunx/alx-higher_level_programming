edec0582ac547a813877580367e1041feabba474
osunx <splendorfreeman1@gmail.com>
2023-11-01 09:05:38 -0700

    Python if else and loop functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 # Print the ASCII alphabet in lowercase without a new line
-# the ASCII value can also be used "for char in range(97, 123)"
-for char in range(ord('a'), ord('z') + 1):
+# The letters can also be used "for char in range(ord('a'), ord('z') + 1)"
+for char in range(97, 123):
     print("{}".format(chr(char)), end="")
