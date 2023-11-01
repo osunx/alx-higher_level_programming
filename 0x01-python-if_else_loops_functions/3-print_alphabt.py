1d78bf1421e34e85ba6b48c5b4646bed8de16f3a
osunx <splendorfreeman1@gmail.com>
2023-11-01 09:15:31 -0700

    Python if else and loop functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 
 # Print the ASCII alphabet in lowercase except for 'e' and 'q'
 for char in range(97, 123):
-    if chr(char) not in 'e' and chr(char) not in 'q':
+    if chr(char) != 'e' and chr(char) != 'q':
         print("{}".format(chr(char)), end="")
