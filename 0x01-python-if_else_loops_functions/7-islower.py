baa5892a615d7cf24fdddf5aa976bb1a1d32a427
osunx <splendorfreeman1@gmail.com>
2023-11-01 09:41:21 -0700

    Python if else and loop functions

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     """Checks if the given character is lowercase"""
-    if c >= 'a' and c <= 'z':
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
-# This can also be done using the ASCII values directly
-#def islower(letter):
- #   """Checks if the given letter is lowercase"""
- #   if ord(letter) >= 97 and ord(letter) <= 122:
- #       return True
- #   else:
- #       return False
+# This can also be done using the char directly c >= 'a' and c <= 'z':
