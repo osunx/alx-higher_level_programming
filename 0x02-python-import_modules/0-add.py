8360440238e2b8fbd6f5ad8aeef43710279340fe
osunx <splendorfreeman1@gmail.com>
2023-11-02 12:34:16 -0700

    Python module

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """ Print the sum of 1 and 2. """
+    from add_0 import add
+
+    n = 1
+    u = 2
+    m = add(n, u)
+
+    print("{} + {} = {}".format(n, u, m))
