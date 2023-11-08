c815a78e8a07bd8258d8a34dc46a6bc72f6d7561
osunx <splendorfreeman1@gmail.com>
2023-11-08 22:18:27 +0000

    Python data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    """Write a function that replaces all occurrences of an element by another in a new list."""
+    """
+    Write a function that replaces all occurrences
+    of an element by another in a new list
+    """
 
     new_list = []
     for element in my_list:
