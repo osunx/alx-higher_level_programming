eac2bd4dc316d35333e0c1f5299956fc0f0a1651
osunx <splendorfreeman1@gmail.com>
2023-11-08 23:56:23 +0000

    Python data structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,7 +2,9 @@
 
 def only_diff_elements(set_1, set_2):
     """
-    Write a function that returns a set of all elements present in only one set.
+    Write a function that returns a
+    set of all elements present in only one set.
+    this is a short solution return '(set_1 ^ set_2)'
     """
 
     rset = set()
@@ -10,4 +12,9 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if i not in set_2:
             rset.add(i)
+
+    for j in set_2:
+        if j not in set_1:
+            rset.add(j)
+
     return rset
